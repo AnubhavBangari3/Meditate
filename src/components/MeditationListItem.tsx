d0f0956b517ff6682a3b93594c47944a5f7c2ec5
Anubhav Bangari <bangarianubhav@gmail.com>
2025-03-10 22:49:49 +0530

    gui

--- src/components/MeditationListItem.tsx
+++ src/components/MeditationListItem.tsx
@@ -1,17 +1,28 @@
 import  {Text , View, FlatList} from 'react-native';
 import { Meditation } from '../types';
 import "../../global.css"
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+
 export function MeditationListItem({meditation}:{meditation :Meditation}){
 
      
 
          return (
-          <View className="p-5 m-5 border-2 border-gray-300 rounded-2xl">
-          <Text className="font-semibold text-4xl">
-         {meditation.title}   
-         </Text>
-         <Text className="text-gray-600"> {meditation.duration}  </Text>
+         <View className="flex-row items-center gap-5">
+          <View className="bg-green-700 p-1 rounded-full">
+           <Entypo name="check" size={16} color="white" />
+          </View>
+           <View className="flex-1 p-5 py-10 border-2 border-gray-300 rounded-2xl">
+                              <Text className="font-semibold text-4xl">
+                    {meditation.title}   
+                    </Text>
+                    <View className="flex-row items-center gap-1">
+                                        <AntDesign name="clockcircleo" size={16} color="dingray" />
+                                        <Text className="text-gray-600"> {meditation.duration} min </Text>
+                    </View>
 
-</View>
+            </View>
+         </View>
          )
 }
