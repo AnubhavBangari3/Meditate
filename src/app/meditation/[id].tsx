0ba3252dd37b0c04b8bcd6392fd664c5824a0112
Anubhav Bangari <bangarianubhav@gmail.com>
2025-03-14 20:43:14 +0530

    design

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,11 +1,13 @@
 import {Text} from 'react-native';
-import { useLocalSearchParams,router } from 'expo-router';
+import { useLocalSearchParams,router,Link } from 'expo-router';
 import { meditations } from '../../data';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function MeditationDetails(){
           const {id} =useLocalSearchParams<{id:string}>();
           const meditation = meditations.find((m) => m.id === Number(id));
+          const {top}= useSafeAreaInsets();
 
           if (!meditation){
                     return <Text>Meditation not found !</Text>
@@ -14,9 +16,12 @@ export default function MeditationDetails(){
 
           return(
                     
-                    <>
-                    <AntDesign onPress = {() => router.back()} className="absolute top-16 right-10" name="close" size={24} color="black" />
+                    <SafeAreaView>
+                    <Link href="../">
+                    <AntDesign onPress = {() => router.back()} style={{top:top+10}} className="absolute top-16 right-10" name="close" size={24} color="black" />
                     <Text className="text-3xl mt-16">Meditation Details : {meditation?.title}</Text>
-                    </>
+                    </Link>
+                    
+                    </SafeAreaView>
           )
 }
